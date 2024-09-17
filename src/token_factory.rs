d3f6d321ada3ba2bb75c342020bae4fdb8ba8e5a
Huskyto Gonar <huskygonar@gmail.com>
2024-09-16 23:00:40 -0400

    Add to token_factory

--- src/token_factory.rs
+++ src/token_factory.rs
@@ -7,38 +7,65 @@ struct TokenFactory {
 
 impl TokenFactory {
     
-    fn from_lexeme(lexeme: &str) -> Token {
+    fn from_lexeme(lexeme: &str) -> Result<Token, String> {
         let token_type = Self::get_token_type(lexeme);
-        Token::new(token_type, lexeme.to_string())
+        if token_type == TokenType::Undef {
+            Err(format!("Error: Unexpected character: {lexeme}"))
+        }
+        else if token_type == TokenType::String {
+            if lexeme.ends_with("\"") {
+                Ok(Token::new_lit(token_type, lexeme.to_string(), lexeme[1..lexeme.len() - 1].to_string()))
+            }
+            else {
+                Err("Error: Unterminated string.".to_string())
+            }
+        }
+        else {
+            Ok(Token::new(token_type, lexeme.to_string()))
+        }
     }
 
     fn get_token_type(lexeme: &str) -> TokenType {
-        match lexeme {
-            "(" => TokenType::LeftParen,
-            ")" => TokenType::RightParen,
-            "[" => TokenType::LeftBracket,
-            "]" => TokenType::RightBracket,
-            "{" => TokenType::LeftBrace,
-            "}" => TokenType::RightBrace,
+        if lexeme.starts_with("//") {
+            TokenType::Empty
+        }
+        else if lexeme.starts_with("\"") {
+            TokenType::String
+        }
+        // else if lexeme.chars().next().unwrap().is_ascii_digit() {
+        //     TokenType::Number
+        // }
+        // else if lexeme.chars().next().unwrap().is_ascii_alphabetic() || lexeme.starts_with('_') {
+        //     TokenType::Identifier
+        // }
+        else {
+            match lexeme {
+                "(" => TokenType::LeftParen,
+                ")" => TokenType::RightParen,
+                "[" => TokenType::LeftBracket,
+                "]" => TokenType::RightBracket,
+                "{" => TokenType::LeftBrace,
+                "}" => TokenType::RightBrace,
 
-            "+" => TokenType::Plus,
-            "-" => TokenType::Minus,
-            "*" => TokenType::Star,
+                "+" => TokenType::Plus,
+                "-" => TokenType::Minus,
+                "*" => TokenType::Star,
 
-            "=" => TokenType::Equals,
-            "==" => TokenType::EqualsEquals,
-            "!=" => TokenType::NotEquals,
-            "!" => TokenType::Bang,
-            "<" => TokenType::Less,
-            ">" => TokenType::Greater,
-            "<=" => TokenType::LessEqual,
-            ">=" => TokenType::GreaterEqual,
+                "=" => TokenType::Equals,
+                "==" => TokenType::EqualsEquals,
+                "!=" => TokenType::NotEquals,
+                "!" => TokenType::Bang,
+                "<" => TokenType::Less,
+                ">" => TokenType::Greater,
+                "<=" => TokenType::LessEqual,
+                ">=" => TokenType::GreaterEqual,
 
-            // "" => TokenType::String,
-            // "" => TokenType::Integer,
-            // "" => TokenType::Float,
+                // "" => TokenType::String,
+                // "" => TokenType::Integer,
+                // "" => TokenType::Float,
 
-            _ => TokenType::Undef
+                _ => TokenType::Undef
+            }
         }
     }
 }
